2d82ecbb9d40af4cabbf3cba3161f114a472f823
Ghrafkly <69286273+Ghrafkly@users.noreply.github.com>
2023-01-17 14:53:53 +1100

    done sorta

--- src/main/java/org/countdownJava/Current/Postfix.java
+++ src/main/java/org/countdownJava/Current/Postfix.java
@@ -9,67 +9,6 @@ public class Postfix {
 	private final AtomicInteger numPostfix = new AtomicInteger();
 
 	public void execute(Map<List<Integer>, List<List<Integer>>> mapCombinationsPermutations) throws ExecutionException, InterruptedException {
-//		versionOneOne(mapCombinationsPermutations);
-//		versionOneTwo(mapCombinationsPermutations);
-		versionTwo(mapCombinationsPermutations);
-//		versionThree(mapCombinationsPermutations);
-	}
-
-	public void versionOneOne(Map<List<Integer>, List<List<Integer>>> mapCombinationsPermutations) throws ExecutionException, InterruptedException {
-		for (List<Integer> combination : mapCombinationsPermutations.keySet()) {
-			Map<Operations, Integer> intermidiarySolutions = new ConcurrentHashMap<>();
-			List<CompletableFuture<Void>> futures = new ArrayList<>();
-
-			for (List<Integer> permutation : mapCombinationsPermutations.get(combination)) {
-				futures.add(CompletableFuture.supplyAsync(() -> {
-					PostfixGen postfixGen = new PostfixGen(permutation);
-					int[][] postfix = postfixGen.generatePostfix();
-					numPostfix.addAndGet(postfix.length);
-
-					return postfix;
-				}).thenAcceptAsync((postfix) -> {
-					int[] solutions = evaluatePostfix(postfix, intermidiarySolutions);
-					for (int solution : solutions) {
-						solutionsMap.merge(solution, 1L, Long::sum);
-					}
-				}));
-			}
-
-			for (CompletableFuture<Void> future : futures) {
-				future.get();
-			}
-
-			intermidiarySolutions.clear();
-		}
-	}
-
-	public void versionOneTwo(Map<List<Integer>, List<List<Integer>>> mapCombinationsPermutations) throws ExecutionException, InterruptedException {
-		List<CompletableFuture<Void>> futures = new ArrayList<>();
-
-		for (List<Integer> combination : mapCombinationsPermutations.keySet()) {
-			Map<Operations, Integer> intermidiarySolutions = new ConcurrentHashMap<>();
-
-			for (List<Integer> permutation : mapCombinationsPermutations.get(combination)) {
-				futures.add(CompletableFuture.runAsync(() -> {
-					PostfixGen postfixGen = new PostfixGen(permutation);
-					int[][] postfix = postfixGen.generatePostfix();
-					numPostfix.addAndGet(postfix.length);
-
-					for (int solution : evaluatePostfix(postfix, intermidiarySolutions)) {
-						solutionsMap.merge(solution, 1L, Long::sum);
-					}
-				}));
-			}
-
-			intermidiarySolutions.clear();
-		}
-
-		for (CompletableFuture<Void> future : futures) {
-			future.get();
-		}
-	}
-
-	public void versionTwo(Map<List<Integer>, List<List<Integer>>> mapCombinationsPermutations) throws ExecutionException, InterruptedException {
 		List<CompletableFuture<Void>> futures = new ArrayList<>();
 
 		for (List<Integer> combination : mapCombinationsPermutations.keySet()) {
@@ -81,9 +20,7 @@ public class Postfix {
 					int[][] postfix = postfixGen.generatePostfix();
 					numPostfix.addAndGet(postfix.length);
 
-					for (int solution : evaluatePostfix(postfix, intermidiarySolutions)) {
-						solutionsMap.merge(solution, 1L, Long::sum);
-					}
+					evaluatePostfix(postfix, intermidiarySolutions).forEach(solution -> solutionsMap.merge(solution, 1L, Long::sum));
 				}
 			}));
 		}
@@ -93,51 +30,8 @@ public class Postfix {
 		}
 	}
 
-	public void versionThree(Map<List<Integer>, List<List<Integer>>> mapCombinationsPermutations) throws ExecutionException, InterruptedException {
-		ExecutorService executor = Executors.newFixedThreadPool(12);
-		Map<List<Integer>, Future<int[][]>> futures = new HashMap<>();
-		List<Future<int[]>> solutions = new ArrayList<>();
-
-		int count = 0;
-		for (Map.Entry<List<Integer>, List<List<Integer>>> combination : mapCombinationsPermutations.entrySet()) {
-			for (List<Integer> permutation : combination.getValue()) {
-				PostfixGen postfixGen = new PostfixGen(permutation);
-				futures.put(permutation, executor.submit(() -> postfixGen.generatePostfix()));
-			}
-
-			Map<Operations, Integer> intermidiarySolutions = new ConcurrentHashMap<>();
-			for (Map.Entry<List<Integer>, Future<int[][]>> future : futures.entrySet()) {
-				solutions.add(executor.submit(() -> evaluatePostfix(future.getValue().get(), intermidiarySolutions)));
-			}
-
-			// add solutions to solutionsMap
-			for (Future<int[]> solution : solutions) {
-				try {
-					for (int sol : solution.get()) {
-						solutionsMap.merge(sol, 1L, Long::sum);
-					}
-				} catch (ExecutionException e) {
-					System.out.println(count);
-				}
-			}
-
-//
-//			// clear solutions to save memory
-			futures.clear();
-			solutions.clear();
-			intermidiarySolutions.clear();
-
-			System.out.printf("Finished %d/%d combinations\r", count++, mapCombinationsPermutations.size());
-		}
-
-
-		// shutdown executors
-		executor.shutdownNow();
-	}
-
-	public int[] evaluatePostfix(int[][] postfixArray, Map<Operations, Integer> intermidiarySolutions) {
-		int[] solutions = new int[150000];
-		int solutionsIndex = 0;
+	public ArrayList<Integer> evaluatePostfix(int[][] postfixArray, Map<Operations, Integer> intermidiarySolutions) {
+		ArrayList<Integer> solutionsList = new ArrayList<>();
 
 		for (int[] postfix : postfixArray) {
 			boolean valid = true;
@@ -169,14 +63,13 @@ public class Postfix {
 						switch (token) {
 							case -1 -> result = a + b;
 							case -3 -> result = a * b;
-
+							// If result is negative, equation is not valid
 							case -2 -> {
-								// If result is negative, equation is not valid
 								if (a > b) result = a - b;
 								else valid = false;
 							}
+							// If result is not an integer, equation is not valid
 							case -4 -> {
-								// If result is not an integer, equation is not valid
 								if (a % b == 0) result = a / b;
 								else valid = false;
 							}
@@ -186,22 +79,18 @@ public class Postfix {
 					}
 
 					stack[stackIndex++] = result;
-//					intermidiarySolutions.put(op, result);
 				    intermidiarySolutions.merge(op, result, (x, y) -> {
 				        if (x.equals(y)) return x;
 				        else return -1;
 					});
 
 					// Add solutions if between 101 and 999 (inclusive)
-					if (unique && result >= 101 && result <= 999) {
-						solutions[solutionsIndex++] = result;
-					}
+					if (unique && result >= 101 && result <= 999) solutionsList.add(result);
 				}
 			}
 		}
 
-		// removes nulls from solutions
-		return Arrays.stream(solutions).filter(i -> i != 0).toArray();
+		return solutionsList;
 	}
 
 
