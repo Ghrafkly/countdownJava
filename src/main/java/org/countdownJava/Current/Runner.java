dbe8cd8aa039657c1345be86b0cd7248d2742b10
Ghrafkly <69286273+Ghrafkly@users.noreply.github.com>
2023-09-18 15:35:01 +1000

    clean up

--- src/main/java/org/countdownJava/Current/Runner.java
+++ src/main/java/org/countdownJava/Current/Runner.java
@@ -56,25 +56,6 @@ public class Runner {
 		counts();
 //		solutions();
 
-//		System.out.printf("Combinations: %d | Permutations: %d | Postfix: %d | Solutions: %d | Time: %d ms%n",
-//				numCombinations, numPermutations, numPostfix, numSolutions, endTime - startTime);
-
-//		combTime = TimeUnit.MILLISECONDS.convert(combTime, TimeUnit.NANOSECONDS);
-//		permTime = TimeUnit.MILLISECONDS.convert(permTime, TimeUnit.NANOSECONDS);
-//		postfixTime = TimeUnit.MILLISECONDS.convert(postfixTime, TimeUnit.NANOSECONDS);
-//
-//		if (combTime > 1000) {
-//			combTime = TimeUnit.SECONDS.convert(combTime, TimeUnit.NANOSECONDS);
-//		}
-//
-//		if (permTime > 1000) {
-//			permTime = TimeUnit.SECONDS.convert(permTime, TimeUnit.NANOSECONDS);
-//		}
-//
-//		if (postfixTime > 1000) {
-//			postfixTime = TimeUnit.SECONDS.convert(postfixTime, TimeUnit.NANOSECONDS);
-//		}
-
 		if (combTime > 1000000000) {
 			System.out.println("Converted combTime to seconds");
 			combTime = TimeUnit.SECONDS.convert(combTime, TimeUnit.NANOSECONDS);
@@ -107,18 +88,6 @@ public class Runner {
 			System.out.println("Converted postfixTime to microseconds");
 			postfixTime = TimeUnit.MICROSECONDS.convert(postfixTime, TimeUnit.NANOSECONDS);
 		}
-
-//		System.out.printf("""
-//				Combinations: %d | Time: %d ms
-//				Permutations: %d | Time: %d ms
-//				Postfix: %d | Time: %d ms
-//				Solutions: %d
-//				""",
-//				numCombinations, combTime,
-//				numPermutations, permTime,
-//				numPostfix, postfixTime,
-//				numSolutions
-//				);
 
 		System.out.printf("""
 				Combinations: %d | Time: %d
